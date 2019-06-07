5909604eb85f4dec27d85f8546da9a9ba4784158
Tim Pittman <timpittman@tims-mbp.attlocal.net>
2019-06-06 21:11:36 -0500

    prod file update

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ config :network, NetworkWeb.Endpoint,
   force_ssl: true,
   loac_from_system_env: true,
   url: [host: "infinite-depths-83240", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
