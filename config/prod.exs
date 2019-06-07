edb782f97c8c078f3a5cfd86cb490b47f2ba3e03
Tim Pittman <timpittman@tims-mbp.attlocal.net>
2019-06-06 20:40:41 -0500

    update buildpack

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :network, NetworkWeb.Endpoint,
-  url: [host: "limitless-cove-48833", port: 443],
+  url: [host: "infinite-depths-83240", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
