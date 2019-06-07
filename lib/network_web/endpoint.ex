aee1ab19825b626640d90643630581502a4ad165
Tim Pittman <timpittman@tims-mbp.attlocal.net>
2019-06-06 22:36:28 -0500

    endpoint

--- lib/network_web/endpoint.ex
+++ lib/network_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule NetworkWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :network
 
   socket "/socket", NetworkWeb.UserSocket,
-    websocket: true
+    websocket: true,
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
